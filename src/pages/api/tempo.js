2b22a2a438abd6ee504d173a93675f65298fe635
jonas mendes <jonas.jsm2903@gmail.com>
2023-09-21 14:49:11 -0300

    cache de 10 segundos

--- src/pages/api/tempo.js
+++ src/pages/api/tempo.js
@@ -5,8 +5,11 @@ async function tempo(request, response) {
     const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiKey}`)
     const dynamicDate = new Date();
     const inscritos = subscribersResponse.total_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxage=10 , stale-while-revalidate');
+
     response.json({
-        date: dynamicDate.getFullYear(),
+        date: dynamicDate.getDate(),
         inscritos: inscritos
     })
 }
